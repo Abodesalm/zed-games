fbacbb6baeced68a72ff7789454a60a4e8f54e65
abod <abodesalm45@gmail.com>
2024-07-23 06:24:44 +0300

    some changes

--- sections/Container.tsx
+++ sections/Container.tsx
@@ -12,25 +12,24 @@ export default async function Container({ endpoint, searchParams, pageUrl }) {
   const rank = `${searchParams.rank?`rank=${searchParams.rank}`:``}`
   const searchStr = `${searchParams.search?searchParams.search:''}`
   const lastEndpoint = `${endpoint}?${rank}${pageStr}`
-  const data = await fetch(lastEndpoint,{next:{revalidate:300}}).then(res => res.json()).then(data => data).catch(err=>console.log(err.response.data.message));
+  const data = await fetch(lastEndpoint,{next:{revalidate:300}}).then(res => res.json()).then(data => data).catch(err=>console.log(err.response));
   //
   (function filterSearch(){
-    data.data.data = data.data.data.filter((el)=>{
+    if(data){
+      data.data.data = data.data.data.filter((el)=>{
       return el.name.toLowerCase().includes(searchStr.toLowerCase())
     })
+    }
   })()
 
   return (
     <>
       <Search />
       <div className="pad min-h-[70vh] w-full flex justify-between items-center flex-wrap gap-8 ">
-        {
+        {data&&
           data.data.data.map((game) => {
             return <Card name={game.name} id={game.id} key={game.id} />
           })
-          /* axios({method:'get',url:`${api}/api/v1/games`}).then(res =>
-          res.data.data.data.map((game)=>{
-            return <Card name={game.name} id={game.id} key={game.id} />})).catch(err=><div>{err.cause}</div>) */
         }
       </div>
       <Pageination url={pageUrl} results={data.results} />
