a2ff795222bcecaa1f383ae137bc5a42bcc0c79b
abod <abodesalm45@gmail.com>
2024-08-17 12:41:43 +0300

    some changes

--- components/Genre.tsx
+++ components/Genre.tsx
@@ -1,4 +1,4 @@
-export default function Genre({ title, clas }) {
+export default function Genre({ title, clas = "" }) {
   return (
     <div
       className={`inline px-1 bg-accent text-dark rounded-[5px] text-[14px] font-medium ${clas}`}
